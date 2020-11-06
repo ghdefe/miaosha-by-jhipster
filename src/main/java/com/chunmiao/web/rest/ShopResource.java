27663cf969c036792ffed5b842257a28092eaf5b
ghdefe <ggdefe@gmail.com>
2020-11-06 19:16:38 +0800

    增加生产者，生产者功能正常。消费者功能还未完成

--- src/main/java/com/chunmiao/web/rest/ShopResource.java
+++ src/main/java/com/chunmiao/web/rest/ShopResource.java
@@ -1,7 +1,14 @@
 package com.chunmiao.web.rest;
 
+import com.chunmiao.config.KafkaProperties;
 import com.chunmiao.service.GoodService;
 import com.chunmiao.service.dto.GoodDTO;
+import org.apache.kafka.clients.Metadata;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.redisson.Redisson;
 import org.redisson.api.RAtomicLong;
 import org.redisson.api.RedissonClient;
@@ -9,10 +16,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.SendResult;
+import java.time.Duration;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.Future;
 
 /**
  * ShopResource controller
@@ -25,19 +37,30 @@ public class ShopResource {
 
     @Autowired
     private GoodService goodService;
+
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
     /**
-    * POST good
-    */
+     * POST good
+     */
     @PostMapping("/good")
     public String good() {
         return "good";
     }
 
     /**
-    * POST goodWithActivity
-    */
+     * POST goodWithActivity
+     */
     @PostMapping("/good-with-activity")
-    public String goodWithActivity(Long goodId,Long activityId) {
+    public String goodWithActivity(Long goodId, Long activityId) {
+        try (
+            KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProperties.getProducerProps())
+        ) {
+            Future<RecordMetadata> shop = producer.send(new ProducerRecord<>("shop", "hello!"));
+        }
+
+
         String buyer = SecurityContextHolder.getContext().getAuthentication().getName();
         RedissonClient redissonClient = Redisson.create();
         RAtomicLong atomicLong = redissonClient.getAtomicLong("stock_" + goodId);
@@ -46,18 +69,29 @@ public class ShopResource {
         if (stock >= 0) {
 //            template.send("test","123");
             return buyer + "购买" + goodId + "成功";
-        }
-        else {
+        } else {
             return "没库存";
         }
     }
 
     /**
-    * DELETE good
+     * DELETE good
+     *
      * @return
      */
     @DeleteMapping("/good")
     public void refund() {
+
+    }
+
+// 消费者
+    @GetMapping("/test")
+    public void consumer(){
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(kafkaProperties.getConsumerProps());
+        LinkedList<String> topics = new LinkedList<>();
+        topics.add("shop");
+        consumer.subscribe(topics);
+        ConsumerRecords<String, String> poll = consumer.poll(Duration.ZERO);
 
     }
 
